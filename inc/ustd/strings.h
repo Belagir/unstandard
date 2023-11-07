da820a3a361bff6ae57d7ff809948429763bb52b
Belagir <belagir.tedab@gmail.com>
2023-11-07 16:46:43 +0100

    robustness on ranges

--- inc/ustd/strings.h
+++ inc/ustd/strings.h
@@ -9,7 +9,11 @@
 #define string range_of(STRING_TYPE)
 #define string_static(__length) range_static(__length, STRING_TYPE)
 
+<<<<<<< HEAD
 #define string_from(__s) range_static_create_fit(STRING_TYPE, __s)
+=======
+#define string_create_static(__s) range_static_create(count_of(((string_base_type[]) {__s})) - 1, string_base_type, __s)
+>>>>>>> 99e8f45 (robustness on ranges)
 
 #define string_val(__s, __i) range_val(__s, __i, STRING_TYPE)
 
