3b28a756093595b40ca87e71c82c3eb994e89dfd
Belagir <belagir.tedab@gmail.com>
2023-11-19 13:52:11 +0100

    ttree lifetime

--- inc/ustd/tree.h
+++ inc/ustd/tree.h
@@ -2,12 +2,16 @@
 #ifndef __TEEMING_TREE_H__
 #define __TEEMING_TREE_H__
 
+#include <ustd/allocation.h>
+
 /**
  * @brief
  *
  */
 typedef enum ttree_mishap {
 	TTREE_NO_MISHAP,
+
+    TTREE_INVALID_OBJECT,
 } ttree_mishap;
 
 /**
@@ -16,7 +20,28 @@ typedef enum ttree_mishap {
  */
 typedef struct ttree ttree;
 
+typedef struct ttree_path {
+    ttree *target;
+    range *tokens_indexes;
+} ttree_path;
 
+/**
+ * @brief Creates a tree of a with an allocator.
+ *
+ * @param[inout] alloc
+ * @param[in] capacity
+ * @param[in] element_size
+ * @return ttree*
+ */
+ttree *ttree_create(allocator alloc, size_t capacity, size_t element_size);
+
+/**
+ * @brief Destroys a tree.
+ *
+ * @param[inout] alloc
+ * @param[inout] tree
+ */
+ttree_mishap ttree_destroy(allocator alloc, ttree **tree);
 
 #ifdef UNITTESTING
 void ttree_execute_unittests(void);
--- src/trees/ttree.c
+++ src/trees/ttree.c
@@ -1,7 +1,6 @@
 
-// #include <stdlib.h>
-
 #include <ustd/common.h>
+#include <ustd/range.h>
 #include <ustd/tree.h>
 
 #ifdef UNITTESTING
@@ -12,13 +11,108 @@
 // -------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------
 
+// -------------------------------------------------------------------------------------------------
+typedef struct ttree {
+    range *tree_contents;
+    range *tree_children_count;
+} ttree;
+
+// -------------------------------------------------------------------------------------------------
+ttree *ttree_create(allocator alloc, size_t capacity, size_t element_size)
+{
+    ttree *tree = { };
+
+    if ((capacity == 0u) || (element_size == 0u)) {
+        return NULL;
+    }
+
+    tree = alloc.malloc(alloc, sizeof(*tree));
+
+    if (tree) {
+        tree->tree_contents       = range_dynamic_create(alloc, element_size, capacity);
+        tree->tree_children_count = range_dynamic_create(alloc, sizeof(size_t), capacity);
+    }
+
+    return tree;
+}
+
+// -------------------------------------------------------------------------------------------------
+ttree_mishap ttree_destroy(allocator alloc, ttree **tree)
+{
+    if (!tree || !*tree) {
+        return TTREE_INVALID_OBJECT;
+    }
+
+    range_dynamic_destroy(alloc, (*tree)->tree_contents);
+    range_dynamic_destroy(alloc, (*tree)->tree_children_count);
+
+    alloc.free(alloc, *tree);
+    *tree = NULL;
+
+    return TTREE_NO_MISHAP;
+}
+
+
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+
 #ifdef UNITTESTING
 
+tst_CREATE_TEST_SCENARIO(tree_lifetime,
+        {
+            size_t capacity;
+            size_t element_size;
+
+            bool expect_success;
+        },
+        {
+            ttree *tree = ttree_create(make_system_allocator(), data->capacity, data->element_size);
+
+            if (data->expect_success) {
+                tst_assert((tree != NULL), "tree creation failed !");
+            } else {
+                tst_assert((tree == NULL), "tree was unexpectedly successful !");
+            }
+
+            if (tree) {
+                ttree_destroy(make_system_allocator(), &tree);
+            }
+
+
+            tst_assert((tree == NULL), "tree was not set to NULL at the end of test");
+        }
+)
+
+tst_CREATE_TEST_CASE(tree_lifetime_u32, tree_lifetime,
+        .capacity       = 16,
+        .element_size   = sizeof(u32),
+        .expect_success = true,
+)
+tst_CREATE_TEST_CASE(tree_lifetime_1ko, tree_lifetime,
+        .capacity       = 2048u,
+        .element_size   = 1024u,
+        .expect_success = true,
+)
+tst_CREATE_TEST_CASE(tree_lifetime_bad_size, tree_lifetime,
+        .capacity       = 16,
+        .element_size   = 0,
+        .expect_success = false,
+)
+tst_CREATE_TEST_CASE(tree_lifetime_bad_count, tree_lifetime,
+        .capacity       = 0,
+        .element_size   = sizeof(long),
+        .expect_success = false,
+)
 
 // -------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------
 void ttree_execute_unittests(void)
 {
+    tst_run_test_case(tree_lifetime_u32);
+    tst_run_test_case(tree_lifetime_1ko);
+    tst_run_test_case(tree_lifetime_bad_size);
+    tst_run_test_case(tree_lifetime_bad_count);
 }
 
 #endif
