b02d923bee00aa3fb2db349c6c06c3f139cc9aaa
Belagir <belagir.tedab@gmail.com>
2023-11-19 11:36:54 +0100

    nuking ttree module

--- inc/ustd/tree.h
+++ inc/ustd/tree.h
@@ -2,34 +2,13 @@
 #ifndef __TEEMING_TREE_H__
 #define __TEEMING_TREE_H__
 
-#include <ustd/common.h>
-#include <ustd/allocation.h>
-#include <ustd/range.h>
-
 /**
  * @brief
  *
  */
 typedef enum ttree_mishap {
 	TTREE_NO_MISHAP,
-
-	TTREE_INCORRECT_PATH
 } ttree_mishap;
-
-typedef enum subttree_part {
-    SUBTREE_PART_CHILDREN,
-    SUBTREE_PART_PARENTS,
-    SUBTREE_PART_WHOLE,
-} subttree_part;
-
-typedef enum subttree_foreach_direction {
-    SUBTREE_FOREACH_DIRECTION_BOTTOM_UP,
-    SUBTREE_FOREACH_DIRECTION_TOP_DOWN,
-} subttree_foreach_direction;
-
-#ifndef TTREE_MAX_DEPTH
-#define TTREE_MAX_DEPTH (16)
-#endif
 
 /**
  * @brief Opaque type to pass around a tree stored contiguously in an array.
@@ -37,102 +16,7 @@ typedef enum subttree_foreach_direction {
  */
 typedef struct ttree ttree;
 
-typedef struct subttree subttree;
 
-/**
- * @brief
- * TODO : add a hash to identify the original state of the parent tree and invalidate the subtree if it changed
- */
-typedef struct subttree {
-	ttree *parent_tree;     /// pointer to the parent tree. IF NULL, then the subttree is invalid
-	size_t pos;             /// position in the parent tree. The subtree is the data at .parent_tree + .pos
-    range_static(TTREE_MAX_DEPTH, void **) parents;     /// range of the parents as subttrees
-} subttree;
-
-/**
- * @brief Type of a function that can modify nodes of the tree
- *
- */
-typedef void (*node_mutation_function)(void **data, void *additional_args);
-
-/**
- * @brief Creates an array-packed tree on the heap.
- *
- * @param[in] nb_nodes maximum number of nodes held inside the tree
- * @return ttree*
- */
-ttree * ttree_create(allocator alloc, size_t nb_nodes);
-
-/**
- * @brief Returns a subtree from a broader tree by its path. The subtree can then be used with other ttree interface methods to change the tree.
- *
- * @param[in] tree target tree, if it is changed during the lifetime of the subtree, the subtree may not be valid anymore
- * @param[in] node_path path to the subtree, node by node
- * @param[in] node_path_length length of the node path
- * @param[in] node_comparator function able to compare two nodes together
- * @return subttree*
- */
-subttree ttree_get_subtree(ttree *tree, const void *node_path[], size_t node_path_length, i32 (*node_comparator)(const void *node_1, const void *node_2));
-
-/**
- * @brief
- *
- * @param target
- * @param node_path
- * @param node_path_length
- * @param node_comparator
- * @return subttree
- */
-subttree ttree_get_subtree_subtree(subttree target, const void *node_path[], size_t node_path_length, i32 (*node_comparator)(const void *node_1, const void *node_2));
-
-/**
- * @brief Returns the contents of the node at the path described by the traget subtree.
- *
- * @param[in] target traget subtree
- * @return void*
- */
-void * ttree_get_subtree_content(subttree target);
-
-/**
- * @brief
- *
- * @param target
- * @return size_t
- */
-size_t ttree_get_subtree_node_count(subttree target);
-
-/**
- * @brief Inserts a new node (by shallow copy) into the tree, next to the target subtree.
- *
- * @param[inout] target target subtree
- * @param[in] node added node
- * @return i32 : 0 if the node was effectively added, > =1 if something went wrong
- */
-i32 ttree_insert(subttree target, void *node);
-
-/**
- * @brief Applies a function to all nodes in a subtree (depth first).
- *
- * @param[inout] target target subtree
- * @param[in] apply_f applied function
- * @param[in] additional_args pointer to additional arguments passed to the applied function
- */
-void ttree_foreach(subttree target, void (*apply_f)(void **node, void *additional_args), void *additional_args, subttree_part part, subttree_foreach_direction direction);
-
-/**
- * @brief Removes a subtree from the tree.
- *
- * @param[inout] target target subtree
- * @return i32 : 0 if the node was effectively removed, > =1 if something went wrong
- */
-i32 ttree_remove(subttree target);
-
-/**
- * @brief Releases the tree from the heap and nullifies the pointer to it.
- *
- * @param[inout] tree discarded tree
- */
-void ttree_destroy(allocator alloc, ttree **tree);
 
 #ifdef UNITTESTING
 void ttree_execute_unittests(void);
