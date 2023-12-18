193d6f9f745a204d6402ca04a33bd003c9ee4d02
Belagir <belagir.tedab@gmail.com>
2023-12-18 21:15:33 +0100

    documenting the allocation module & the common module

--- inc/ustd/allocation.h
+++ inc/ustd/allocation.h
@@ -1,3 +1,13 @@
+/**
+ * @file allocation.h
+ * @author gabriel
+ * @brief Provides declarations for the custom allocators used widely in the library.
+ * Custom allocators permit finer control over what is happening with dynamic memory, allowing different methods of management to be used in a program. If the user has the discipline to always inject the allocators through arguments up to the context that will use the allocator's methods, it is trivial to recognize which parts of the code mess with dynamic memory at a glance. This has the downside of having the developper needing to remember what allocator created what object.
+ * @version 0.1
+ * @date 2023-12-18
+ *
+ * @copyright Copyright (c) 2023
+ */
 
 #ifndef __ALLOCATION_H__
 #define __ALLOCATION_H__
@@ -15,7 +25,7 @@ typedef struct allocator {
 } allocator;
 
 /**
- * @brief Builds an allocator based on the system's malloc() and free().
+ * @brief Builds an allocator based on the system's malloc() and free() from stdlib.
  *
  * @return allocator
  */
@@ -23,6 +33,8 @@ allocator make_system_allocator(void);
 
 /**
  * @brief Builds an allocator from some memory. The allocator will reside in the supplied memory, and so its lifetime is linked to the memory's lifetime.
+ * This allocator is very fast, but is vulnerable to out of bound writes and fragmentation (who thought !). Note that the allocator will use some of the passed memory for itself, so not all bytes that are given to the allocator will be available to be allocated.
+ *
  *
  * @param[inout] mem memory the allocator can use
  * @param[in] length length in bytes of the memory region
