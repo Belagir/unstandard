320b3e09004c8d1161fa8ae5c46cd694286d8545
Belagir <belagir.tedab@gmail.com>
2024-11-24 12:46:16 +0100

    forgot about test module

--- src/testing/testutilities.c
+++ src/testing/testutilities.c
@@ -6,7 +6,7 @@
 #include <time.h>
 
 // -------------------------------------------------------------------------------------------------
-void __tst_print(char *message, ...) {
+void tstprivate_print(char *message, ...) {
     va_list args;
     va_start(args, message);
 
@@ -16,7 +16,7 @@ void __tst_print(char *message, ...) {
 }
 
 // -------------------------------------------------------------------------------------------------
-int __tst_compare_mem(void *addr1, void *addr2, unsigned long size_bytes) {
+int tstprivate_compare_mem(void *addr1, void *addr2, unsigned long size_bytes) {
     char *byte_addr1 = (char *) addr1;
     char *byte_addr2 = (char *) addr2;
     char *byte_end = ((char *) addr1) + size_bytes;
@@ -33,10 +33,10 @@ int __tst_compare_mem(void *addr1, void *addr2, unsigned long size_bytes) {
 }
 
 // -------------------------------------------------------------------------------------------------
-int __tst_print_test_case_report(char *case_name, unsigned nb_assertions, unsigned nb_failed, const char *file, int line) {
+int tstprivate_print_test_case_report(char *case_name, unsigned nb_assertions, unsigned nb_failed, const char *file, int line) {
     if (nb_failed == 0) {
-        __tst_print("\033[1;32m[TEST PASSED]\033[0m : %s:%d \033[1m`%s'\033[0m with %d assertions ran.\n", file, line, case_name, nb_assertions);
+        tstprivate_print("\033[1;32m[TEST PASSED]\033[0m : %s:%d \033[1m`%s'\033[0m with %d assertions ran.\n", file, line, case_name, nb_assertions);
     } else {
-        __tst_print("\033[1;31m[TEST FAILED]\033[0m : %s:%d \033[1m`%s'\033[0m with %d assertions failed out of %d ran.\n", file, line, case_name, nb_failed, nb_assertions);
+        tstprivate_print("\033[1;31m[TEST FAILED]\033[0m : %s:%d \033[1m`%s'\033[0m with %d assertions failed out of %d ran.\n", file, line, case_name, nb_failed, nb_assertions);
     }
 }
